ba69c01f50e568d04aa62908028f8e7ef6bf9795
Hortencia_Arliane <hortenciaxavierifrn@gmail.com>
2020-04-24 00:20:56 -0300

    Adicionando o script.js feito na aula 1 e 2 do whorkshop sem erros.

---
+++ script.js
@@ -0,0 +1,25 @@
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide");
+	}
+
+
+		$(document).ready(function(){
+  		$("#addIdeia").click(function(){
+    	$("#container").toggle();
+  });
+});
+
+	$(document).ready(function(){
+  		$("#voltarIdeia").click(function(){
+    	$("#container").toggle();
+  });
+});
+
+	$(document).ready(function(){
+  		$("#salvarIdeia").click(function(){
+    	$("#container").toggle();
+  });
+});
